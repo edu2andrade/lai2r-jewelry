86739e364600ba72a3a66b686f6485f039b04681
Eduardo Andrade <edu2andrade@gmail.com>
2021-12-07 14:26:57 +0100

    working on tailwind

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      Hello World!
+    <div className="flex items-center h-screen justify-center bg-platinum">
+      <div className="flex items-center h-64 w-1/2 justify-evenly gap-2 bg-salmon">
+        <div className="w-24 h-12 bg-darkGray text-center text-platinum rounded-md m-2">1</div>
+        <div className="w-24 h-12 bg-darkGray text-center text-platinum rounded-md m-2">2</div>
+        <div className="w-24 h-12 bg-darkGray text-center text-platinum rounded-md m-2">3</div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      platinum: '#e8e6e2ff',
+      darkGray: '#363e3fff',
+      rawUmber: '#7c6643ff',
+      camel: '#c18e46ff',
+      eerieBlack: '#18191aff',
+      rawUmber2: '#89654fff',
+      salmon: '#fab084ff',
+    },
     extend: {},
   },
   variants: {
