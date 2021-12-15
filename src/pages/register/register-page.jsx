61667d3f1d85a0fcfd9e8d8a3e012328ace969f7
Eduardo Andrade <edu2andrade@gmail.com>
2021-12-15 12:53:17 +0100

    sign in and register pages created separately with all files organized

---
+++ src/pages/register/register-page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Register from "../../components/register/register.component";
+
+const RegisterPage = () => (
+  <div className='mt-24'>
+    <Register />
+  </div>
+)
+
+export default RegisterPage;
