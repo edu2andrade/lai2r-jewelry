27db1da1a3a7f867f7d696abdbc18d220ac9a96b
Eduardo Andrade <edu2andrade@gmail.com>
2021-12-18 12:45:14 +0100

    cart-item component added

---
+++ src/components/cart/cart-item.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CartItem = ({ item: { imageUrl, price, name, quantity }}) => (
+  <div className='cart-item'>
+    <img src={imageUrl} alt="item_added" />
+    <div className='item-details'>
+      <span className='name'>{ name }</span>
+      <span className='price'>{ quantity } x €{ price }</span>
+    </div>
+  </div>
+)
+
+export default CartItem;
