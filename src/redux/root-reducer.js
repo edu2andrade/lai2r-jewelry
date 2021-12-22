1d13deb3c09d57faa0fb8fbef523763f86b69926
Eduardo Andrade <edu2andrade@gmail.com>
2021-12-22 12:07:07 +0100

    redux-persist for localStorage added

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,21 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // localStorage!
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 import hamburguerReducer from "./hamburguer/hamburguer.reducer";
 
-export default combineReducers({
-  user: userReducer, 
+const persistConfig = {
+  key: "root",
+  storage,
+  whiteList: ["cart"],
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
   cart: cartReducer,
-  hamburguer: hamburguerReducer
-})
+  hamburguer: hamburguerReducer,
+});
+
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,18 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+// redux-persist --> local and session Storage, allows our browser to actually caches the store
+import { persistStore } from "redux-persist";
 
 import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares))
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+// Create a new 'persisted' version of our store
+export const persistor = persistStore(store);
+
+// assign the object to a variable before export
+const storePersistor = { store, persistor };
+
+export default storePersistor;
